5e77c111a7b72cc6788c5bbab9a4863f88bac2eb
Alex Lee <alex@alex-lee.site>
2019-05-17 10:26:57 -0700

    Split types into a Types folder.

--- src/MegaCard/Types/Card.js
+++ src/MegaCard/Types/Card.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import { Box, Text, Heading } from 'grommet'
-import {
-  buildShadow,
-  colors,
-  defaultProps,
-  icons,
-  propTypes
-} from '../../constants'
+import { Box } from 'grommet'
+import { buildShadow, defaultProps, propTypes } from '../../constants'
+import Indicator from '../Indicator'
+import Content from '../Content'
+import Icon from '../Icon'
 
 const Card = ({
   background,
@@ -19,8 +16,6 @@ const Card = ({
   title,
   titleColor = color
 }) => {
-  const Icon = icons[icon]
-
   return (
     <Box
       align="center"
@@ -33,43 +28,9 @@ const Card = ({
       round="medium"
       style={{ boxShadow: buildShadow(shadowSize, shadowType) }}
     >
-      {indicator && (
-        <Box>
-          <Box
-            height="8px"
-            round="full"
-            style={{ backgroundColor: colors[color] }}
-            width="8px"
-          />
-        </Box>
-      )}
-      <Box align="start" margin={{ right: 'medium' }}>
-        <Text size="16px" style={{ color: colors[titleColor] }} weight="bold">
-          {title}
-        </Text>
-        <Heading
-          color="dark-2"
-          level="5"
-          margin={{ bottom: 'none', top: 'xsmall' }}
-          style={{ fontSize: '1.25rem' }}
-        >
-          {data}
-        </Heading>
-      </Box>
-      {icon && (
-        <Box>
-          <Box
-            align="center"
-            height="42px"
-            justify="center"
-            round="full"
-            style={{ backgroundColor: colors[color] }}
-            width="42px"
-          >
-            <Icon color="white" size="28px" />
-          </Box>
-        </Box>
-      )}
+      {indicator && <Indicator color={color} />}
+      <Content data={data} title={title} titleColor={titleColor} />
+      {icon && <Icon color={color} icon={icon} />}
     </Box>
   )
 }
