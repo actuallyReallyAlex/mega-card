65f5471e057b4a80ced34a5a78202358928c2d0b
Alex Lee <alex@alex-lee.site>
2019-06-12 21:39:29 -0700

    Add initial test.

---
+++ src/tests/MegaCard.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import MegaCard from '../index'
+import { shallow } from 'enzyme'
+
+const defaultProps = {
+  data: 'Test Data',
+  title: 'Title'
+}
+
+describe('<MegaCard />', () => {
+  afterEach(() => {
+    // defaultProps.handler.mockClear()
+  })
+
+  test('Should render the <MegaCard /> component as a card.', () => {
+    const component = shallow(<MegaCard {...defaultProps} type="card" />)
+    expect(component).toMatchSnapshot()
+  })
+
+  test('Should render the <MegaCard /> component as a pill.', () => {
+    const component = shallow(<MegaCard {...defaultProps} type="pill" />)
+    expect(component).toMatchSnapshot()
+  })
+})
